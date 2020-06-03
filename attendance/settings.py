cf931ee9c82e8510e65c93762f64d38dcea7316f
Arsh Jain <arshjain99@gmail.com>
2020-06-04 02:52:46 +0530

    modified settings.py

--- attendance/settings.py
+++ attendance/settings.py
@@ -28,7 +28,7 @@ with open("attendance/etc/secret_key.txt") as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["young-hollows-15148.herokuapp.com"]
 
 
 # Application definition
